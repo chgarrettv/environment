dad5e5857d8bbc174557947d84fbba96cc41f8c7
chgarrettv <claudehgarrettv@hotmail.com>
2025-08-27 22:41:47 +0000

    [tmux] Update the BHDL repo path.

--- .tmux/bhdl.sh
+++ .tmux/bhdl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export MYTMUXPRESET="~/.tmux/bhdl.sh"
-tmux new-session -s "BHDL" \; \
-  neww -n "Repository" -c ~/Repositories/BHDL \; \
-  neww -n "Editor" -c ~/Repositories/BHDL \; \
+tmux new-session -s "bhdl" \; \
+  neww -n "Repository" -c ~/Repositories/bhdl \; \
+  neww -n "Editor" -c ~/Repositories/bhdl \; \
   select-window -t 0 \;
